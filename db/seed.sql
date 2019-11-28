9a41fd613362ed4b594f6f2fd1a95785bf9975ab
GabrielTMangum <gabrieltmangum@gmail.com>
2019-11-28 12:11:05 -0500

    initial commit

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Shack Stack');
+INSERT INTO burgers (burger_name) VALUES ('Five Guys Burger');
