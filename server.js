febe74a0d7ef75e9345eff9a75205f213e258a3a
GabrielTMangum <gabrieltmangum@gmail.com>
2019-11-28 22:44:12 -0500

    finished most of server.js and began work on script.js

--- server.js
+++ server.js
@@ -1,2 +1,61 @@
-const express = require("express");
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
 
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "rootroot",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+app.get("/", function (req, res) {
+    connection.query("SELECT * FROM burgers", (err, data) => {
+        if (err) throw err;
+
+        res.render("index", { burgers: data })
+    })
+});
+
+app.put("/api/burgers/:id", function(req, res) {
+    const id = req.params.id;
+    const updatedBurger = req.body;
+  
+    connection.query(
+      "UPDATE quotes SET ? WHERE ?", [updatedurger, {id: id}], (err, data) => {
+      if (err) throw err;
+  
+      res.end();
+    })
+  });
+
+app.post("/api/burgers", function (req, res) {
+    const newBurger = req.body;
+
+    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [newBurger.burger_name], (err, data) => {
+        if (err) throw err;
+        res.end();
+    })
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
